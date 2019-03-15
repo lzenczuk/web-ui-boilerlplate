c0f10497900bc0486ed645cd124af64f8bc796e2
lzenczuk <lucjan.zenczuk@gmail.com>
2019-03-15 16:49:31 +0000

    Add /src/index.html as template file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
